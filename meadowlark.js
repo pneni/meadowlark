3a53dc44ac60b415be7d7197491107e259d974ef
pneni <neni.padma@tesglobal.com>
2016-01-12 22:10:54 +0000

    Added qa test

--- meadowlark.js
+++ meadowlark.js
@@ -16,8 +16,15 @@ var handlebars = require('express-handlebars') .create({ defaultLayout:'main' })
     app.engine('handlebars', handlebars.engine);
     app.set('view engine', 'handlebars');
 
+app.use(function(req,res,next){
+	res.locals.showTests = app.get('env')!=='production'&& req.query.test==='1';
+	next();
+});
+
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
+
+// Routes go here.....
 
 app.get('/', function(req, res){
 	res.render('home');
@@ -25,13 +32,27 @@ app.get('/', function(req, res){
 	// res.send ('MEADOW LARK TRAVEL');
 });
 
-app.get('/about', function(req,res){
-//var randomFortune =  fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about' ,{ fortune: fortune.getFortune() });
+app.get('/about', function(req,res) {
+	//var randomFortune =  fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
 	// res.type('text/plain');
 	// res.send('ABOUT MEADOW LARK TRAVEL');
 });
 
+app.get('/tours/hood-river', function(req,res){
+	 res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req,res){
+	res.render('tours/request-group-rate');
+});
+
+app.get('/tours/oregon-coast',function(req,res){
+	res.render('tours/oregon-coast');
+});
 
 //Custome 404 Page
 
@@ -49,6 +70,8 @@ app.use(function(err,req,res,next){
 	// res.type('text/plain');
 	// res.send('500 - server error');
 });
+
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:'+app.get('port')+'; press ctrl+c to terminate');
 });
+
